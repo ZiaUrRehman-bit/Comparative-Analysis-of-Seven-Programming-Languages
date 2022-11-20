af7ddb4dc5e17d88c49a818be365ca0b73f59865
Zia Ur Rehman <77435711+ZiaUrRehman-bit@users.noreply.github.com>
2022-11-20 15:22:35 +0500

    Create CalculateSum.cpp

---
+++ CalculateSum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+int main() {
+    
+    int iterations = 10000000;
+    double x = 0;
+    
+    clock_t time_req;
+
+    time_req = clock();
+
+    for (int i = 0; i < iterations; i++)
+    {
+        x = x + i;
+    }
+    time_req = clock() - time_req;
+    cout<<x<<"\n";
+    cout<<(float) time_req/CLOCKS_PER_SEC);
+
+    return 0;
+}
